b1c44d44c0b0842611e81ca1bb7f6ce9e7f97505
HitandFeyd <cchavez572@cnm.edu>
2024-01-25 13:24:16 -0700

    Add project files.

---
+++ CChavezStudentClassDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CChavezStudentClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            myStudent.SetMajor("Computer Programing");
+
+            Student myOtherStudent = new Student();
+            myOtherStudent.SetMajor("Art History");
+
+            Console.WriteLine("My student's major is {0}.", myStudent.GetMajor);
+            Console.WriteLine("My other student's major is {0}.", myOtherStudent.GetMajor);
+        }
+    }
+}
---
+++ CChavezStudentClassDemo/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChavezStudentClassDemo
+{
+    internal class Student
+    {
+        // FIELDS
+        private string major;
+        private int score1;
+        private int score2;
+        private int score3;
+        private string studentFirstName;
+        private string studentLastName;
+        private string studentNumber;
+
+        // ACCESSOR
+        public string GetMajor()
+        {
+            return major;
+        }
+
+        public int GetScore1()
+        {
+            return score1;
+        }
+
+        public int GetScore2()
+        {
+            return score2;
+        }
+
+        public int GetScore3()
+        {
+            return score3;
+        }
+        public string GetStudentFirstName()
+        {
+            return studentFirstName;
+        }
+        public string GetStudentLastName()
+        {
+            return studentLastName;
+        }
+
+        public string GetStudentNumber()
+        {
+            return studentNumber;
+        }
+
+        // MUTATOR FUNCTIONS
+        // ALWAYS VOID
+        public void SetMajor(string newMajor)
+        {
+            major = newMajor;
+        }
+        public void SetScore1(int newScore1)
+        {
+            score1 = newScore1;
+        }
+
+        public void SetScore2(int newScore2)
+        {
+            score1 = newScore2;
+        }
+        public void SetScore3(int newScore3)
+        {
+            score1 = newScore3;
+        }
+
+        // CONSTRUCTORS
+        public Student()
+        {
+            // empty body can be used for
+        }
+    }
+
+
+}
